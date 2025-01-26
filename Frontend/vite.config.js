975fb097f0355e3a631b907f17e7af1fd8f4b069
Rares Baciu <rares@Raress-MacBook-Air.local>
2025-01-26 11:55:29 +0200

    updated dependencies

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11']
+    })
+  ],
+  server: {
+    headers: {
+      'Content-Security-Policy': "script-src 'self' 'unsafe-inline';"
+    }
+  }
+});
