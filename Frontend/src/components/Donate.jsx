26f6adfebece5ecb054c4b62a454dd00c33cb275
Rares Baciu <rares@Raress-MacBook-Air.local>
2025-01-26 20:36:04 +0200

    changes

--- Frontend/src/components/Donate.jsx
+++ Frontend/src/components/Donate.jsx
@@ -3,8 +3,8 @@ import { ethers } from 'ethers';
 import PlatformManagerABI from '../abi/PlatformManager.json';
 
 export function Donate() {
-  const [totalDonated, setTotalDonated] = useState(0); // Exemplu de total donat
-  const [isSectionVisible, setIsSectionVisible] = useState(true); // Starea vizibilității secțiunii
+  const [totalDonated, setTotalDonated] = useState(0);
+  const [isManager, setIsManager] = useState(false);
   const [provider, setProvider] = useState(null);
   const [signer, setSigner] = useState(null);
   const [platformManager, setPlatformManager] = useState(null);
@@ -15,12 +15,16 @@ export function Donate() {
         const newProvider = new ethers.providers.Web3Provider(window.ethereum);
         await newProvider.send('eth_requestAccounts', []);
         const newSigner = newProvider.getSigner();
-        console.log(newSigner);
         const newPlatformManager = new ethers.Contract(
           '0x5FbDB2315678afecb367f032d93F642f64180aa3',
           PlatformManagerABI.abi,
           newSigner
         );
+        const contractCode = await newProvider.getNetwork();
+        const managerAddress = await newPlatformManager.manager();
+        const userAddress = await newSigner.getAddress();
+        setIsManager(managerAddress.toLowerCase() === userAddress.toLowerCase());
+
         setProvider(newProvider);
         setSigner(newSigner);
         setPlatformManager(newPlatformManager);
@@ -34,7 +38,7 @@ export function Donate() {
   const handleDonate = async () => {
     if (!platformManager) return;
 
-    const donationAmount = ethers.utils.parseEther('0.1'); // Suma de donat (exemplu)
+    const donationAmount = ethers.utils.parseEther('0.1');
     try {
       const tx = await platformManager.donate({ value: donationAmount });
       await tx.wait();
@@ -46,10 +50,26 @@ export function Donate() {
     }
   };
 
+  const handleDistributeFunds = async () => {
+    if (!platformManager) return;
+
+    try {
+      const tx = await platformManager.distributeFunds();
+      await tx.wait();
+      alert('Funds distributed successfully!');
+    } catch (error) {
+      console.error('Distribution failed', error);
+      alert('Failed to distribute funds');
+    }
+  };
+
   return (
     <div className="donate-container">
       <h1>Donate Page</h1>
       <button onClick={handleDonate}>Donate</button>
+      <button style={{ marginLeft: '20px', marginTop: '20px' }} onClick={handleDistributeFunds}>
+        Distribuie fondurile
+      </button>
       <div style={{ marginTop: '20px' }}>
         <p>Total Donated: {totalDonated} ETH</p>
       </div>
